d4ccc6f44370be9947a27b3d62115f84c7b72533
alinaseem123 <alinaseem21102002@gmail.com>
2026-01-04 13:04:54 +0500

    php

--- src/routes/test.routes.js
+++ src/routes/test.routes.js
@@ -7,14 +7,4 @@ const router = Router();
 router.get('/supabase', testSupabase);
 
 module.exports = router; // ✅ export for CommonJS
-
-// Vaporesso Coils & Pods
-// LAW Black 1 1/4 24pk
-// Cannabis Design #48927 By Zippo
-// Geek Vape Coils & Pods   works for details but not for main page
-// 
-// Original Cannoli By BAMS    works for details but not for main page
-// MAG Solo Kit By Smok
-//HY BIRD - Monster Rig Set
-// Vaporesso Coils & Pods
-// NFL New York Jets Design #46459 By Zippo
+//this is php version
