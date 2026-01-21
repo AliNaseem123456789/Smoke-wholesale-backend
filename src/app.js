9f4388988793777817914d856999925af176b74d
alinaseem123 <alinaseem21102002@gmail.com>
2026-01-21 23:35:37 +0500

    Added railway link

--- src/app.js
+++ src/app.js
@@ -22,7 +22,10 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "https://acetonic-humilis-lino.ngrok-free.dev",
+    origin:
+      "https://smoke-wholesale-kxuk-4j95gqy60-ali-naseems-projects-305588ff.vercel.app/",
+    // origin: "http://localhost:5173",
+    // origin: "https://acetonic-humilis-lino.ngrok-free.dev",
     credentials: true,
   }),
 );
