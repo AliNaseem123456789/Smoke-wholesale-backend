99eacf61dc962a4a44a00bcf9cb2d5de3858f85b
alinaseem123 <alinaseem21102002@gmail.com>
2026-01-22 00:33:22 +0500

    fixed cookie

--- src/app.js
+++ src/app.js
@@ -22,8 +22,8 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin:
-      "https://smoke-wholesale-kxuk-4j95gqy60-ali-naseems-projects-305588ff.vercel.app",
+    origin: "https://smoke-wholesale.vercel.app",
+    // "https://smoke-wholesale-kxuk-4j95gqy60-ali-naseems-projects-305588ff.vercel.app",
     // origin: "http://localhost:5173",
     // origin: "https://acetonic-humilis-lino.ngrok-free.dev",
     credentials: true,
